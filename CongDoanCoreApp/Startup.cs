7c77ce5aab2700c89221631db0ea0cf60d53947f
Congdoan97 <congdoan6997@gmail.com>
2018-08-05 23:21:57 +0300

    Sử dụng Area tạo phần Admin trong ASP.NET Core

--- CongDoanCoreApp/Startup.cs
+++ CongDoanCoreApp/Startup.cs
@@ -99,6 +99,11 @@ namespace CongDoanCoreApp
                 routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
+
+                routes.MapRoute(
+                  name: "areas",
+                  template: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+                );
             });
         }
     }
